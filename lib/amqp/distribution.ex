24acb7e715cb2336157f5df67f15405aa37f31d4
Langhaarzombie <mixmax199989@gmail.com>
2018-02-01 15:26:09 +0100

    Added amqp consumer for distributions

---
+++ lib/amqp/distribution.ex
@@ -0,0 +1,64 @@
+defmodule Routing.Distribution do
+  use GenServer
+  use AMQP
+  require Logger
+
+  alias Routing.Routehandler
+
+  def start_link(_opts), do: GenServer.start(__MODULE__, [], [])
+
+  def init(_args), do: connect_rabbitmq()
+
+  @exchange "distributionex"
+  @queue    "distribution"
+  @error    "#{@queue}_error"
+  
+  def connect_rabbitmq do
+    case Connection.open("#{Application.fetch_env!(:routing, :cloudamqp_url)}") do
+      {:ok, conn} ->
+        Process.monitor(conn.pid)
+        {:ok, chan} = Channel.open(conn)
+        setup_queue(chan)
+        Basic.qos(chan, prefetch_count: 100)
+        {:ok, _consumer_tag} = Basic.consume(chan, @queue)
+        {:ok, chan}
+
+      {status, message} ->
+        Logger.warn("Unknown connection state: #{status}, #{message}")
+        :timer.sleep(1000)
+        connect_rabbitmq()
+    end
+  end
+
+  def setup_queue(chan) do
+    Queue.declare(chan, @error, durable: true)
+    Queue.declare(chan, @queue, durable: true)
+    Exchange.direct(chan, @exchange, durable: true) # Declaring the exchange
+    Queue.bind(chan, @queue, @exchange) # Binding the two above
+  end
+
+  # Handling confirmation of registering the consumer
+  def handle_info({:basic_consume_ok, %{consumer_tag: consumer_tag}}, chan) do
+    Logger.info("Consumer #{consumer_tag} registered for exchange #{@exchange} and queue #{@queue}")
+    {:noreply, chan}
+  end
+
+  # Handling unexpected cancelling
+  def handle_info({:basic_cancel, %{consumer_tag: consumer_tag}}, chan) do
+    Logger.info("Connection canceled unexpectedly for consumer #{consumer_tag}")
+    {:stop, :normal, chan}
+  end
+
+  # Handling received message
+  def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: _}}, chan) do
+    Logger.debug("Handling incoming message")
+    Basic.ack(chan, tag)
+    case Routehandler.calc_distribution(payload) do
+      {:err, message} ->
+        Logger.warn(message)
+      {:ok, message} ->
+        Logger.debug(message)
+    end
+    {:noreply, chan}
+  end
+end
