3cb3472c846eef78760c89d61d492e6bb85e7397
Langhaarzombie <mixmax199989@gmail.com>
2018-02-12 20:34:18 +0100

    Adapted to database changes

--- lib/repository/graphrepo.ex
+++ lib/repository/graphrepo.ex
@@ -17,13 +17,13 @@ defmodule Routing.Graphrepo do
       %{:body => b, :headers => _, :status_code => 200} ->
         Logger.debug("Fetching hives succeded with code 200")
         Poison.decode!(~s/#{b}/)
-      %{:body => _, :headers => _, :status_code => s} ->
-        Logger.error("Fetching hives returned a bad status code: #{s}")
+      %{:body => b, :headers => _, :status_code => s} ->
+        Logger.error("Fetching hives returned a bad status code: #{s} with error message #{b}")
     end
   end
   defp get_edges(_shop, _cust) do
     # TODO implement to include Shop and Customer
-    case HTTPotion.get("#{Application.fetch_env!(:routing, :database_url)}/reachable") do
+    case HTTPotion.get("#{Application.fetch_env!(:routing, :database_url)}/api/reachable") do
       %{:body => b, :headers => _, :status_code => 200} ->
         Logger.debug("Fetching hives succeded with code 200")
         Poison.decode!(~s/#{b}/)
@@ -31,9 +31,10 @@ defmodule Routing.Graphrepo do
         Logger.error("Database returned a bad status code: #{s}")
     end
   end
+  # TODO can be made more dry
   defp get_start([], from), do: Logger.error("Start not found in fetched graph #{from}")
   defp get_start([building | t], from) do
-    case Map.get(building, "id") do
+    case Map.get(building, "db/id") do
       ^from ->
         building
       _ ->
@@ -42,7 +43,7 @@ defmodule Routing.Graphrepo do
   end
   defp get_target([], to), do: Logger.error("Destination not found in fetched graph: #{to}")
   defp get_target([building | t], to) do
-    case Map.get(building, "id") do
+    case Map.get(building, "db/id") do
       ^to ->
         building
       _ ->
@@ -60,20 +61,20 @@ defmodule Routing.Graphrepo do
   defp add_nodes([building | t], from, to, graph) do
     # TODO enable for customer and shops because they are not a hive
     heur_costs = round(Distance.GreatCircle.distance(
-      {Map.get(building, "xcoord"), Map.get(building, "ycoord")},
-      {Map.get(to, "xcoord"), Map.get(to, "ycoord")}
+      {Map.get(building, "building/xcoord"), Map.get(building, "building/ycoord")},
+      {Map.get(to, "building/xcoord"), Map.get(to, "building/ycoord")}
     ))
-    hive = Map.get(building, "hive")
+    hive = Map.get(building, "building/hive")
     graph = Graph.add_node(add_nodes(t, from, to, graph),
-                            :"dp#{Map.get(building, "id")}",
-                            %{costs: heur_costs, label: Map.get(hive, "name")})
+                            :"dp#{Map.get(building, "db/id")}",
+                            %{costs: heur_costs, label: Map.get(hive, "hive/name")})
     graph
   end
   defp add_edges([], graph), do: graph
   defp add_edges([edge | t], graph) do
-    from = :"dp#{Map.get(Map.get(edge, "start"), "id")}"
-    to = :"dp#{Map.get(Map.get(edge, "end"), "id")}"
-    costs = round(Map.get(edge, "distance"))
+    from = :"dp#{Map.get(Map.get(edge, "connection/start"), "db/id")}"
+    to = :"dp#{Map.get(Map.get(edge, "connection/end"), "db/id")}"
+    costs = round(Map.get(edge, "connection/distance"))
     graph = Graph.add_edge(add_edges(t, graph), from, to, costs)
     graph
   end
--- lib/repository/routerepo.ex
+++ lib/repository/routerepo.ex
@@ -8,13 +8,14 @@ defmodule Routing.Routerepo do
 
   def insert_route(route) do
     data = transform_to_db_format(route) |> Poison.encode!
-    result = case HTTPotion.post(Application.fetch_env!(:routing, :database_url) <> "/routes", [body: data, follow_redirects: true]) do
-      %{:body => b, :headers => _, :status_code => 200} ->	
+    result = case HTTPotion.post(Application.fetch_env!(:routing, :database_url) <> "/routes",
+                                                        [body: data, headers: ["Content-Type": "application/json"]]) do
+      %{:body => b, :headers => _, :status_code => 201} ->	
         Logger.debug("Route inserted successfully")
         # Transforms to fromat that can instantly be inserted into the redis db
         Poison.decode!(~s/#{b}/) |> transform_to_routing_format
-      %{:body => _, :headers => _, :status_code => s} ->
-        Logger.error("Error #{s} occured trying to insert route #{data}")
+      %{:body => b, :headers => _, :status_code => s} ->
+        Logger.error("Error #{s} occured trying to insert route #{data} on url #{Application.fetch_env!(:routing, :database_url)} with error message #{b}")
     end
     result
   end
@@ -22,20 +23,20 @@ defmodule Routing.Routerepo do
     %{route: hops, time: time, is_delivery: delivery} = route
     result = case delivery do
       true ->
-        %{hops: hops, time: Timex.to_unix(time)*1000, origin: ":route.origin/order"}
+        %{hops: hops, time: Timex.to_unix(time)*1000, origin: "route.origin/order"}
       false ->
-        %{hops: hops, time: Timex.to_unix(time)*1000, origin: ":route.origin/distribution"}
+        %{hops: hops, time: Timex.to_unix(time)*1000, origin: "route.origin/distribution"}
     end
     result
   end
   def transform_to_routing_format(route) do
-    transform_hops(route["_route"], route["id"])
+    transform_hops(route["hop/_route"], route["db/id"])
   end
   def transform_hops([], _route_id), do: []
   def transform_hops([h | t], route_id) do
-    hop = %{dep_time: "#{Timex.from_unix(round(h["starttime"]/1000))}",
-      arr_time: "#{Timex.from_unix(round(h["endtime"]/1000))}",
-      hop: "#{h["id"]}",
+    hop = %{dep_time: "#{Timex.from_unix(round(h["hop/starttime"]/1000))}",
+      arr_time: "#{Timex.from_unix(round(h["hop/endtime"]/1000))}",
+      hop: "#{h["db/id"]}",
       route_id: "#{route_id}"}
     [hop] ++ transform_hops(t, route_id)
   end
