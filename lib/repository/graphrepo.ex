b6dbc065d8b8633bdb3ff47b4dd0a3bce6623f9b
Langhaarzombie <mixmax199989@gmail.com>
2018-02-14 19:56:33 +0100

    - Moved database responsibilities to routehandler - New Order now accepts shopid and customerid - Processing of distributions (with buildingid and buildingid) - Handling of direct distributions and ones that need a route - Handling of different cases for routes - Refactoring
    Signed-off by Langhaarzombie <mixmax199989@gmail.com>

--- lib/repository/graphrepo.ex
+++ lib/repository/graphrepo.ex
@@ -2,16 +2,82 @@ defmodule Routing.Graphrepo do
   require Logger
   use GenServer
 
-  def handle_call({:get_graph_for, from, to}, _from, _state) do
-    Logger.debug("Fetching graph for #{from}, #{to}")
-    hives = get_hives(from, to)
-    edges = get_edges(from, to)
-    s = get_start(hives, String.to_integer(from))
-    t = get_target(hives, String.to_integer(to))
-    graph = transform_to_graph(hives, edges, s, t)
-    {:reply, graph, graph}
+  @url Application.fetch_env!(:routing, :database_url)
+
+  def handle_call({:get_graph_delivery, start, target}, _from, _state) do
+    start = String.to_integer(start)
+    target = String.to_integer(target)
+
+    Logger.debug("Fetching graph for shop: #{start} to customer: #{target}")
+
+    start_building = get_building_for(start, :shops)
+    target_building = get_building_for(target, :customers)
+    graph = build_graph(start_building, target_building)
+
+    {:reply, {graph, start_building["db/id"], target_building["db/id"]}, graph}
   end
-  defp get_hives(_shop, _cust) do
+  
+  def handle_call({:get_graph_distribution, start, target}, _from, _state) do
+    start = String.to_integer(start)
+    target = String.to_integer(target)
+
+    Logger.debug("Fetching graph for distribution building from: #{start} to building to: #{target}")
+
+    start_building = get_building_for(start, :buildings)
+    target_building = get_building_for(target, :buildings)
+    graph = build_graph(start_building, target_building)
+
+    {:reply, {graph, start_building["db/id"], target_building["db/id"]}, graph}
+  end
+
+  defp build_graph(from, to) do
+    hives = get_hives()
+    edges = get_edges(from["db/id"], to["db/id"]) #NOTE workaraound because it only works with building ids for now
+    transform_to_graph(hives, edges, from, to)
+  end
+
+  defp get_building_for(id, table) do
+    case table do
+      :buildings ->
+        result = case HTTPotion.get(@url <> "/one/buildings/#{id}") do
+          %{:body => b, :headers => _, :status_code => 200} ->
+            Logger.debug("Fetching building succeeded with code 200")
+            Poison.decode!(~s/#{b}/)
+          %{:body => b, :headers => _, :status_code => s} ->
+            Logger.error("Fetching building returned a bad status code: #{s} with error message #{b}")
+        end
+      :shops ->
+        shops = case HTTPotion.get(@url <> "/shops") do
+          %{:body => b, :headers => _, :status_code => 200} ->
+            Logger.debug("Fetching shops succeeded with code 200")
+            Poison.decode!(~s/#{b}/)
+          %{:body => b, :headers => _, :status_code => s} ->
+            Logger.error("Fetching shops returned a bad status code: #{s} with error message #{b}")
+        end
+        do_find_entitiy(shops, id, :shop)
+      :customers ->
+        customers = case HTTPotion.get(@url <> "/customers") do
+          %{:body => b, :headers => _, :status_code => 200} ->
+            Logger.debug("Fetching customers succeeded with code 200")
+            Poison.decode!(~s/#{b}/)
+          %{:body => b, :headers => _, :status_code => s} ->
+            Logger.error("Fetching customers returned a bad status code: #{s} with error message #{b}")
+        end
+        do_find_entitiy(customers, id, :customer)
+    end
+  end
+
+  defp do_find_entitiy([], entity, description), do: Logger.error("Could not find entity for id #{entity} and description #{description}")
+  defp do_find_entitiy([h | t], entity, description) do
+    case Map.get(h, "building/#{description}") |> Enum.at(0) |> Map.get("db/id") do
+      ^entity ->
+        h
+      _ -> 
+        do_find_entitiy(t, entity, description)
+    end
+  end
+
+  defp get_hives do
     # TODO implement to include Shop and Customer
     case HTTPotion.get("#{Application.fetch_env!(:routing, :database_url)}/hives") do
       %{:body => b, :headers => _, :status_code => 200} ->
@@ -21,55 +87,40 @@ defmodule Routing.Graphrepo do
         Logger.error("Fetching hives returned a bad status code: #{s} with error message #{b}")
     end
   end
-  defp get_edges(_shop, _cust) do
-    # TODO implement to include Shop and Customer
-    case HTTPotion.get("#{Application.fetch_env!(:routing, :database_url)}/api/reachable") do
+
+  defp get_edges(from, to) do
+    case HTTPotion.get("#{Application.fetch_env!(:routing, :database_url)}/api/reachable?customerid=#{to}&shopid=#{from}") do
       %{:body => b, :headers => _, :status_code => 200} ->
-        Logger.debug("Fetching hives succeded with code 200")
+        Logger.debug("Fetching connections for #{from} and #{to} succeded with code 200")
         Poison.decode!(~s/#{b}/)
-      %{:body => _, :headers => _, :status_code => s} ->
-        Logger.error("Database returned a bad status code: #{s}")
+      %{:body => b, :headers => _, :status_code => s} ->
+        Logger.error("Database returned a bad status code upon fetching connections: #{s} with message #{b}")
     end
   end
-  # TODO can be made more dry
-  defp get_start([], from), do: Logger.error("Start not found in fetched graph #{from}")
-  defp get_start([building | t], from) do
-    case Map.get(building, "db/id") do
-      ^from ->
-        building
-      _ ->
-        get_start(t, from)
-    end
-  end
-  defp get_target([], to), do: Logger.error("Destination not found in fetched graph: #{to}")
-  defp get_target([building | t], to) do
-    case Map.get(building, "db/id") do
-      ^to ->
-        building
-      _ ->
-        get_target(t, to)
-    end
-  end
+
   defp transform_to_graph(hives, edges, from, to) do
     # TODO maybe work with the state of the server instead of creating a new graph
     # (depends on graph brewer if it is able to update an existing graph)
-    graph = add_nodes(hives, from, to, Graph.new)
+    graph = add_nodes(hives, from, to)
     graph = add_edges(edges, graph)
     graph
   end
-  defp add_nodes([], _from, _to, graph), do: graph
+
+  # TODO nil is a workaround to enable first inserting of shop and cust
+  defp add_nodes(hives, from, to) do
+    graph = Graph.add_node(Graph.new, :"dp#{Map.get(to, "db/id")}", %{costs: 0, label: "Start"})
+            |> Graph.add_node(:"dp#{Map.get(from, "db/id")}", %{costs: get_heur_costs_buildings(from, to), label: "End"})
+    add_nodes(hives, from, to, graph)
+  end
+  defp add_nodes([], _, _, graph), do: graph
   defp add_nodes([building | t], from, to, graph) do
     # TODO enable for customer and shops because they are not a hive
-    heur_costs = round(Distance.GreatCircle.distance(
-      {Map.get(building, "building/xcoord"), Map.get(building, "building/ycoord")},
-      {Map.get(to, "building/xcoord"), Map.get(to, "building/ycoord")}
-    ))
     hive = Map.get(building, "building/hive")
-    graph = Graph.add_node(add_nodes(t, from, to, graph),
-                            :"dp#{Map.get(building, "db/id")}",
-                            %{costs: heur_costs, label: Map.get(hive, "hive/name")})
-    graph
+    Graph.add_node(add_nodes(t, from, to, graph), 
+                    :"dp#{Map.get(building, "db/id")}",
+                    %{costs: get_heur_costs_buildings(building, to), label: Map.get(hive, "hive/name")})
   end
+
   defp add_edges([], graph), do: graph
   defp add_edges([edge | t], graph) do
     from = :"dp#{Map.get(Map.get(edge, "connection/start"), "db/id")}"
@@ -79,4 +130,11 @@ defmodule Routing.Graphrepo do
     graph
   end
 
+  defp get_heur_costs_buildings(from, to) do
+    round(Distance.GreatCircle.distance(
+      {Map.get(from, "building/xcoord"), Map.get(from, "building/ycoord")},
+      {Map.get(to, "building/xcoord"), Map.get(to, "building/ycoord")}
+    ))
+  end
 end
+
--- lib/repository/routerepo.ex
+++ lib/repository/routerepo.ex
@@ -1,6 +1,8 @@
 defmodule Routing.Routerepo do
   require Logger
 
+  @url Application.fetch_env!(:routing, :database_url)
+
   def get_real_data(route) do
     data = Poison.encode!(route)
     route
@@ -8,18 +10,29 @@ defmodule Routing.Routerepo do
 
   def insert_route(route) do
     data = transform_to_db_format(route) |> Poison.encode!
-    result = case HTTPotion.post(Application.fetch_env!(:routing, :database_url) <> "/routes",
-                                                        [body: data, headers: ["Content-Type": "application/json"]]) do
+    result = case HTTPotion.post(@url <> "/routes", [body: data, headers: ["Content-Type": "application/json"]]) do
       %{:body => b, :headers => _, :status_code => 201} ->	
         Logger.debug("Route inserted successfully")
         # Transforms to fromat that can instantly be inserted into the redis db
         Poison.decode!(~s/#{b}/) |> transform_to_redis_format
       %{:body => b, :headers => _, :status_code => s} ->
-        Logger.error("Error #{s} occured trying to insert route #{data} on url #{Application.fetch_env!(:routing, :database_url)} with error message #{b}")
+        Logger.error("Error #{s} occured trying to insert route #{data} on url #{@url} with error message #{b}")
     end
     result
   end
-  def transform_to_db_format(route) do
+
+  def is_reachable(building1, building2) do
+    result = case HTTPotion.get(@url <> "/api/reachable/#{building1}/#{building2}") do
+      %{:body => b, :headers => _, :status_code => 200} ->	
+        Logger.debug("Is reachable from #{building1} to #{building2} succeeded with code 200")
+        Poison.decode!(~s/#{b}/)
+      %{:body => b, :headers => _, :status_code => s} ->
+        Logger.error("Error #{s} occured trying to check reachable for #{building1} and #{building2} on url #{@url} with error message #{b}")
+    end
+    result
+  end
+
+  defp transform_to_db_format(route) do
     %{route: hops, time: time, is_delivery: delivery} = route
     result = case delivery do
       true ->
@@ -29,9 +42,11 @@ defmodule Routing.Routerepo do
     end
     result
   end
-  def transform_to_redis_format(route), do: transform_hops(route["hop/_route"], route["db/id"])
-  def transform_hops([], _route_id), do: []
-  def transform_hops([h | t], route_id) do
+
+  defp transform_to_redis_format(route), do: transform_hops(route["hop/_route"], route["db/id"])
+
+  defp transform_hops([], _route_id), do: []
+  defp transform_hops([h | t], route_id) do
     hop = %{dep_time: "#{Timex.from_unix(round(h["hop/starttime"]/1000))}",
       arr_time: "#{Timex.from_unix(round(h["hop/endtime"]/1000))}",
       hop: "#{h["db/id"]}",
