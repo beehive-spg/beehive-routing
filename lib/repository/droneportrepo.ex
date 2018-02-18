87e76d20a4ea5104ca3c6b2688a9ebc97912b970
Langhaarzombie <mixmax199989@gmail.com>
2018-02-18 15:29:13 +0100

    Database query for droneport costs

--- lib/repository/droneportrepo.ex
+++ lib/repository/droneportrepo.ex
@@ -4,20 +4,34 @@ defmodule Routing.Droneportrepo do
   # TODO implement as GenServer
   # Even method paramenters need to be changed for proper implementation
 
+  @url Application.fetch_env!(:routing, :database_url)
+
   def get_predictions_for(buildings, time) when is_list(buildings) do
-    map_predictions(buildings, 1)
+    result = case HTTPotion.get(@url <> "/api/hivecosts?#{build_ids(buildings, time)}") do
+      %{:body => b, :headers => _, :status_code => 200} ->
+        Logger.debug("Fetching costs for hives succeeded with code 200")
+        Poison.decode!(~s/#{b}/)
+      %{:body => b, :headers => _, :status_code => s} ->
+        Logger.error("Fetching hive costs returned a bad status code: #{s} with error message #{b}")
+    end
+    map_predictions(result)
   end
 
-  defp map_predictions([], _value), do: %{}
-  defp map_predictions([h | t], value) do
-    map_predictions(t, value) |> Map.merge(%{h => value})
+  defp build_ids([], time), do: "time=#{time}"
+  defp build_ids([id | t], time) do
+    id = Regex.replace(~r/[A-Za-z]*/, "#{id}", "")
+    "ids=#{id}&" <> build_ids(t, time)
   end
+
+  defp map_predictions([]), do: %{}
+  defp map_predictions([h | t]), do: map_predictions(t) |> Map.merge(%{:"dp#{h["db/id"]}" => h["hive/cost"]})
 
   def get_predicted_cost_factor(data, building, :take) do
     Map.get(data, building)
   end
-  def get_predicted_cost_factor(data, building, :give) do
-    Map.get(data, building)
-  end
 
+  def get_predicted_cost_factor(data, building, :give) do
+    2 - Map.get(data, building) / 20
+  end
 end
+
